0b51d57d314962c83c207cce60eba9977fcd7ddd
Cadu Millet <cadumillet@icloud.com>
2022-06-08 23:44:22 -0300

    first commit

---
+++ src/App.js
@@ -0,0 +1,8 @@
+import "./App.css";
+import LoginForm from "./components/loginForm";
+
+function App() {
+  return <LoginForm />;
+}
+
+export default App;
---
+++ src/components/loginForm.jsx
@@ -0,0 +1,16 @@
+import React, { Component } from "react";
+import ReactDOM from "react-dom";
+
+class LoginForm extends Component {
+  state = {};
+  render() {
+    return (
+      <div>
+        <h1>hello</h1>
+        <button className="contained">odasi</button>
+      </div>
+    );
+  }
+}
+
+export default LoginForm;
